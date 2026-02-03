64b80fcf70b4f66d8427eb5e08507ab46a029b96
Ezechias22 <toussaintezechias16@gmail.com>
2026-02-02 23:10:40 -0300

    fix: export GamesService for PublicModule

--- src/games/games.module.ts
+++ src/games/games.module.ts
@@ -1,19 +1,21 @@
 import { Module } from '@nestjs/common';
 import { GamesController } from './games.controller';
 import { GamesService } from './games.service';
+
 import { SlotFruitStarModule } from './slots/fruit_star/slot.module';
 import { WalletModule } from '../wallet/wallet.module';
 import { EngineModule } from './engine/engine.module';
-import { OperatorModule } from '../operator/operator.module'; // ✅ AJOUTE
+import { OperatorModule } from '../operator/operator.module';
 
 @Module({
   imports: [
-    OperatorModule, // ✅ nécessaire pour OperatorAuthGuard + OperatorService
+    OperatorModule, // nécessaire pour OperatorAuthGuard
     WalletModule,
     EngineModule,
     SlotFruitStarModule,
   ],
   controllers: [GamesController],
   providers: [GamesService],
+  exports: [GamesService], // ✅ LIGNE CLÉ (OBLIGATOIRE)
 })
 export class GamesModule {}
